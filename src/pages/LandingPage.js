98bf1da30e3d51c78f980f2a51be88c316bdd8f5
Farizal hendra Putra Kusuma <43551882+Farizal97@users.noreply.github.com>
2021-09-09 04:40:48 +0700

    text-decoration

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,14 +1,15 @@
-import React from "react";
+/* eslint-disable no-unused-vars */
+import React, { Component } from "react";
 
-import Button from "elements/Button";
+import Header from "parts/Header";
 
-export default class  LandingPage extends Component {
+
+export default class LandingPage extends Component{
     render(){
-    return (
-            <div>
-                
-            </div>
-        )
+        return (
+        <>
+            <Header {...this.props}></Header>
+        </>
+        );
     }
-    
 }
